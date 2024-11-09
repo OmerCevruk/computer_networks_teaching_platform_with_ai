610c99307b500e29bf4ff4b8e60a27e06aec837a
Ömer Çevrük <omer.cevrukk@gmail.com>
2024-11-09 23:26:17 +0300

    fixing admin page to add quizes and answers
    allows adding questions

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,55 +1,7 @@
-# models.py
-from .models import Quiz, Question, Answer, UserQuestionStatus
-from django.utils.html import format_html
-from django.contrib import admin
-from django.db import models
-from django.contrib.auth.models import User
-
-
-class Quiz(models.Model):
-    name = models.CharField(max_length=200)
-    hardness = models.IntegerField()
-
-    def __str__(self):
-        return self.name
-
-
-class Question(models.Model):
-    quiz = models.ForeignKey(
-        Quiz, related_name='questions', on_delete=models.CASCADE)
-    question_number = models.IntegerField()
-    question_text = models.TextField()
-    hardness = models.IntegerField()
-    correct_answer = models.OneToOneField(
-        'Answer',
-        null=True,  # Allow null temporarily
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='correct_for_question'
-    )
-
-    def __str__(self):
-        return f"{self.quiz.name} - Question {self.question_number}"
-
-
-class Answer(models.Model):
-    question = models.ForeignKey(
-        Question, related_name='answers', on_delete=models.CASCADE)
-    answer_text = models.TextField()
-
-    def __str__(self):
-        return self.answer_text
-
-
-class UserQuestionStatus(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    old_answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name_plural = "User Question Statuses"
-
-
 # admin.py
+from django.contrib import admin
+from django.utils.html import format_html
+from .models import Quiz, Question, Answer, UserQuestionStatus
 
 
 class AnswerInline(admin.TabularInline):
