d0aedcf8a19c4d2f15abb343c804777d9ce3663c
Ömer Çevrük <omer.cevrukk@gmail.com>
2024-12-15 00:50:27 +0300

    fixes the bug where style was not applied to base.html

--- networkai/settings.py
+++ networkai/settings.py
@@ -26,9 +26,7 @@ SECRET_KEY = 'django-insecure-4yu^cl9ksoit2+(t3+ku)8z%je+%6&0!gr9_ffm*bh@ua+qkch
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-STATIC_URL = '/static/'
+ALLOWED_HOSTS = ["*", "projebir.omercevruk.com.tr"]
 
 # Application definition
 
@@ -124,7 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
